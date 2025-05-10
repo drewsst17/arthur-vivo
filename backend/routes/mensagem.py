d470d5b63f8cfd01708fe7b14d12012c6959d33c
Andrews Tesolin <ast.rfg@gmail.com>
2025-05-09 21:49:14 -0300

    primeiro deploy

---
+++ backend/routes/mensagem.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request, jsonify
+from models.mensagem import Mensagem
+from database import db
+
+mensagem_bp = Blueprint('mensagem_bp', __name__, url_prefix='/api')
+
+@mensagem_bp.route('/mensagem', methods=['POST'])
+def receber_mensagem():
+    data = request.get_json()
+    msg = data.get('msg', '')
+    if not msg:
+        return jsonify({'erro': 'Mensagem vazia'}), 400
+
+    nova = Mensagem(conteudo=msg)
+    db.session.add(nova)
+    db.session.commit()
+
+    return jsonify({'resposta': 'Mensagem registrada com sucesso 🧠'})
